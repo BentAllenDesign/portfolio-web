384343102c1676045c77d10037337e0b621d1513
Joshua Shevach <jbshevach@cruisr.net>
2023-02-05 14:09:25 -0600

    feat: set up base page with navigation and upper bg

--- src/Routestrap.tsx
+++ src/Routestrap.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+import { Page } from "./components/Page";
 
 export const Routestrap = () => (
   <Routes>
-    <Route path="" element={<></>}> // Page component
+    <Route path="" element={<Page />}> // Page component
       <Route index element={<></>} /> // Landing component
-      <Route path="/resume" element={<></>} /> // Resume component
+      <Route path="/about" element={<></>} /> // About Me component
       <Route path="/projects" element={<></>} /> // Projects component
+      <Route path="/resume" element={<></>} /> // Resume component
       <Route path="/contact" element={<></>} /> // Resume component
     </Route>
   </Routes>
