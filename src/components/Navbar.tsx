2dd68f372fc7d17f417e328ac3814436dc6cfa2f
Joshua Shevach <jbshevach@cruisr.net>
2023-02-07 04:24:23 -0600

    feat: create base layout and setup for the landing page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,39 +3,40 @@ import { Box, Container, IconButton } from "@mui/material";
 import clsx from "clsx";
 import { memo, useCallback } from "react";
 import { Link } from "react-router-dom";
+import { AppThemeContext } from "../lib/hooks/useAppTheme";
 
-const linkStyles = "no-underline block capitalize text-lg text-inherit font-medium transition-all duration-200 ease-in-out drop-shadow-sm hover:text-cyan-500";
+const linkStyles = "no-underline block capitalize text-lg text-inherit font-medium transition-all duration-150 ease-in-out drop-shadow-sm hover:text-cyan-500";
 
 interface INavbar {
-  darkMode: boolean;
-  setDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
+  theme: AppThemeContext;
+  setDarkMode: (darkMode: boolean) => void;
 }
 
-export const Navbar = memo<INavbar>(({ darkMode, setDarkMode }) => {
+export const Navbar = memo<INavbar>(({ theme, setDarkMode }) => {
   const handleThemeChange = useCallback(() => {
-    setDarkMode(!darkMode);
-  }, [darkMode]);
+    setDarkMode(!theme.darkMode);
+  }, [theme.darkMode]);
 
   return (
-    <Box className="flex items-center drop-shadow-md relative">
-      <Box className={clsx("absolute w-full h-full opacity-50 backdrop-blur-lg", darkMode ? 'bg-gray-900' : 'bg-white')} />
-      <Container className="flex items-center py-6">
-        <Box className="font-logo -rotate-6 -mt-7">
+    <Box className="flex items-center drop-shadow-md z-20 fixed w-full top-0 backdrop-blur-sm">
+      <Box className={clsx("absolute w-full h-full opacity-50 backdrop-blur-lg", theme.darkMode ? 'bg-gray-900' : 'bg-white')} />
+      <Container className="flex items-center py-3">
+        <Box className="font-logo -rotate-6 -mt-[34px]">
           <Link to="/" replace className="group no-underline text-transparent">
-            <span className={clsx("fixed z-50 subpixel-antialiased tracking-tighter text-4xl font-logo", darkMode ? 'text-gray-200' : 'text-gray-900')}>
+            <span className={clsx("select-none fixed z-50 subpixel-antialiased tracking-tighter text-4xl font-logo", theme.darkMode ? 'text-gray-200' : 'text-gray-900')}>
               JOSHE
             </span>
             <span className={clsx(
-              "absolute subpixel-antialiased tracking-tighter text-4xl font-logo transition-all duration-300 ease-in-out translate-x-[1px] translate-y-[3px]",
+              "select-none absolute subpixel-antialiased tracking-tighter text-4xl font-logo transition-all duration-300 ease-in-out translate-x-[1px] translate-y-[3px]",
               "group-hover:translate-x-[3px] group-hover:translate-y-[5px] group-hover:opacity-95 group-hover:scale-105 opacity-40",
-              darkMode ? 'text-gray-300 group-hover:text-cyan-500' : 'text-gray-900 group-hover:text-cyan-400'
+              theme.darkMode ? 'text-gray-300 group-hover:text-cyan-500' : 'text-gray-900 group-hover:text-cyan-500'
             )}
             >
               JOSHE
             </span>
           </Link>
         </Box>
-        <Box className={clsx("font-display tracking-tight flex space-x-4 ml-[150px]", darkMode ? 'text-gray-400' : 'text-gray-900')}>
+        <Box className={clsx("select-none tracking-tight flex space-x-4 ml-[150px]", theme.darkMode ? 'text-zinc-400' : 'text-zinc-800')}>
           <Link to="/resume" className={linkStyles}>
             resume
           </Link>
@@ -49,8 +50,8 @@ export const Navbar = memo<INavbar>(({ darkMode, setDarkMode }) => {
             about
           </Link>
         </Box>
-        <IconButton onClick={handleThemeChange} className={clsx("ml-auto transition duration-100", darkMode ? 'text-gray-300' : 'text-gray-900')}>
-          {darkMode
+        <IconButton onClick={handleThemeChange} className={clsx("ml-auto transition duration-100", theme.darkMode ? 'text-zinc-400 hover:text-zinc-200' : 'text-zinc-800 hover:text-zinc-600')}>
+          {theme.darkMode
             ? <LightMode fontSize="large" className="text-inherit" />
             : <DarkMode fontSize="large" className="text-inherit" />}
         </IconButton>
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,3 +1,53 @@
+import { ChevronRightRounded, GitHub, Instagram, LinkedIn, Twitter } from "@mui/icons-material";
+import { Box, Button, Container, IconButton } from "@mui/material";
+import clsx from "clsx";
+import { Link } from "react-router-dom";
+import { useAppTheme } from "../lib/hooks/useAppTheme"
+
 export const LandingPage = () => {
-  
+  const { darkMode } = useAppTheme();
+
+  return (
+    <Container>
+      <Box className="flex flex-col lg:flex-row gap-8">
+        <Box className="flex rounded-full overflow-clip h-44 w-44 min-w-36 drop-shadow self-center sm:self-start">
+          <img src="/headshot.jpg" alt="Joshua Shevach - headshot photo" className="object-cover h-48 w-56 self-center scale-150" />
+        </Box>
+        <Box className="max-w-2xl">
+          <h1 className={clsx("text-4xl font-bold font-display tracking-tight sm:text-5xl mt-0", darkMode ? 'text-zinc-100' : 'text-zinc-800')}>
+            Full-time software engineer.
+            <br />
+            Part-time travel pro.
+          </h1>
+          <p className={clsx("mt-6 text-base", darkMode ? 'text-zinc-300' : 'text-zinc-700')}>
+            I’m Josh, a full-stack software engineer and travel agent based in Frisco Texas. I’m the owner of
+            Cruisr LLC, a cruise travel agency I created to book cheaper cruises for my friends, family, and
+            colleagues! In my free time, I like to game, binge shows, and hone my engineering skills by building
+            bots and small full-stack apps.
+          </p>
+          <Box className="-ml-3 mt-3 flex sm:gap-2">
+            <IconButton
+              href="https://www.linkedin.com/in/joshua-shevach/"
+              aria-label="Follow on LinkedIn"
+              className={clsx("transition-all duration-100 ease-in-out", darkMode ? 'text-zinc-400 hover:text-zinc-200' : 'text-zinc-800 hover:text-zinc-600')}
+            >
+              <LinkedIn className="text-4xl sm:text-5xl" />
+            </IconButton>
+            <IconButton
+              href="https://github.com/BentAllenDesign"
+              aria-label="Follow on GitHub"
+              className={clsx("transition-all duration-100 ease-in-out", darkMode ? 'text-zinc-400 hover:text-zinc-200' : 'text-zinc-800 hover:text-zinc-600')}
+            >
+              <GitHub className="text-4xl sm:text-5xl" />
+            </IconButton>
+            <Button endIcon={<ChevronRightRounded />} className={clsx("ml-3 normal-case rounded-full font-bold text-base px-4 sm:px-6 my-2", darkMode ? 'bg-zinc-400 hover:bg-zinc-200 text-slate-800' : 'bg-zinc-800 hover:bg-zinc-600 text-slate-100')}>
+              <Link to="/about" className="text-inherit no-underline">
+                Learn more about me!
+              </Link>
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
